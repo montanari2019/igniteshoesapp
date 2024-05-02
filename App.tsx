62a177165d6c349046186500a223aceb844eb2f5
montanari2019 <ikaro.montanari31@gmail.com>
2024-05-01 23:37:11 -0400

    boa noite

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-
+import  { OneSignal } from "react-native-onesignal"
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize('6c1f6c86-ceea-4ea3-989c-bd948c1afd32')
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,48 @@ import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
 import { AppRoutes } from './app.routes';
+import { useEffect, useState } from 'react';
+import { NotificationWillDisplayEvent, OSNotification, OneSignal } from 'react-native-onesignal';
+import { Notification } from '../components/Notification';
 
 export function Routes() {
   const { colors } = useTheme();
 
+  const [notification, setNotification ] = useState<OSNotification>()
+
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
+
+  useEffect(()=>{
+
+    const handleNotifications = (event: NotificationWillDisplayEvent): void =>{
+      event.preventDefault()
+      const response = event.getNotification()
+
+      setNotification(response)
+    }
+
+    OneSignal.Notifications.addEventListener(
+      "foregroundWillDisplay",
+      handleNotifications
+    )
+
+
+    return () =>{
+        OneSignal.Notifications.removeEventListener(
+          'foregroundWillDisplay',
+          handleNotifications
+        )
+    }
+
+  },[])
 
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+     {
+      notification?.body && ( <Notification onClose={() => setNotification(undefined)} title={notification.body}/> )
+     }
     </NavigationContainer>
   );
 }
